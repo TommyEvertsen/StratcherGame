329e7aa7ece9f055752cde83598db22fad99ac67
Tommy Evertsen <tommy.evertsen@hjelseth.com>
2024-08-10 19:52:30 +0200

    made_dice_enum

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -1,17 +1,24 @@
 public enum DiceRoll {
 
-ONE,
-TWO,
-THREE,
-FOUR,
-FIVE,
-SIX,
-SEVEN,
-EIGHT,
-NINE,
-TEN,
-ELEVEN,
-TWELVE;
+ONE("You rolled 1"),
+TWO("You rolled 2"),
+THREE("You rolled 3"),
+FOUR("You rolled 4r"),
+FIVE("You rolled 5"),
+SIX("You rolled 6 "),
+SEVEN("You rolled 7"),
+EIGHT("You rolled 8"),
+NINE("You rolled 9"),
+TEN("You rolled 10"),
+ELEVEN("You rolled 11"),
+TWELVE("You rolled 12 CRITICAL!");
+
+final String diceRollText;
+
+DiceRoll(String diceRollText){
+    this.diceRollText = diceRollText;
+}
+
 
 
 }
