c47818ba70c0ed80b7f7401505ff24ec4871c0dd
Tommy Evertsen <tommy.evertsen@hjelseth.com>
2024-08-11 14:45:12 +0200

    Make dice, enemies etc

---
+++ src/Stratcher.java
@@ -0,0 +1,9 @@
+
+public class Stratcher extends Enemies {
+
+    @Override
+    public void defeated() {
+        System.out.println("Dont be witty, dont be billier, dont be unproppa!");
+    }
+
+}
